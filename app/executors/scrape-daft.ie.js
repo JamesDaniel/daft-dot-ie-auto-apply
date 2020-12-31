0d26f225f9dff45018f48ab2b1939ed07a752496
JamesDaniel <jamesmcgarr@protonmail.com>
2020-12-31 23:14:32 +0000

    Updated scraper to work against daft.ie

--- app/executors/scrape-daft.ie.js
+++ app/executors/scrape-daft.ie.js
@@ -29,7 +29,7 @@ async function visitPage(config) {
                 height: 900,
                 deviceScaleFactor: 1,
             });
-            await page.goto('https://www.rent.ie/houses-to-let/renting_limerick/limerick-city-centre/2_beds/rent_0-1000/');
+            await page.goto('https://www.daft.ie/property-for-rent/limerick-city-centre-limerick?rentalPrice_to=1000&numBeds_from=2&sort=publishDateDesc&rentalPrice_from=500');
             await page.click('button[data-tracking=cc-accept]');
             await page.waitForTimeout(1000);
 
@@ -42,7 +42,7 @@ async function visitPage(config) {
                 const apartment = {
                     linkText: await getLinkText(page, link),
                     linkUrl: await getLinkUrl(page, link),
-                    isDoubleRoom: await isDouble(page, searchResults[i])
+                    isDoubleRoom: await isDouble(browser, await getLinkUrl(page, link))
                 }
                 apartments.push(apartment);
             }
@@ -51,16 +51,15 @@ async function visitPage(config) {
 
             const dataSaved = saveIfNotExists(config, apartments);
             if (dataSaved.length > 0) {
-                sendMsg('emailListener', 'screenScraper', JSON.stringify(dataSaved))
+                sendMsg(`${config.appId}emailListener`, 'screenScraper', JSON.stringify(dataSaved))
                     .then(() => {
                         console.log('Data sent to emailer');
                     });
-                sendMsg('smsListener', 'screenScraper', JSON.stringify(dataSaved))
+                sendMsg(`${config.appId}smsListener`, 'screenScraper', JSON.stringify(dataSaved))
                     .then(() => {
                         console.log('Data sent to sms sender');
                     });
             }
-
 
             await page.waitForTimeout(2000);
         } catch (error) {
@@ -78,23 +77,44 @@ async function getLink(searchResult) {
 }
 
 async function getLinkText(page, link) {
-    let value = await page.evaluate(el => el.textContent, link)
+    let linkTextElement = await link.$$('[data-testid=address]');
+    let value = await page.evaluate(el => el.textContent, linkTextElement[0])
     return value.replace(/^\s+|\s+$/g, '');
 }
 
 async function getLinkUrl(page, link) {
     let value = await page.evaluate(el => el.getAttribute('href'), link)
-    return value.replace(/^\s+|\s+$/g, '');
+    return `https://www.daft.ie${value.replace(/^\s+|\s+$/g, '')}`;
 }
 
 async function getSearchResults(page) {
-    await page.waitForSelector('.search_result')
-    return await page.$$('.search_result')
+    await page.waitForSelector('[data-testid=results] > li')
+    return await page.$$('[data-testid=results] > li')
 }
 
-async function isDouble(page, searchResult) {
-    let value = await page.evaluate(el => el.textContent, searchResult)
-    return value.includes('2 double');
+async function isDouble(browser, linkUrl) {
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1800,
+        height: 900,
+        deviceScaleFactor: 1,
+    });
+    await page.goto(linkUrl);
+    await page.waitForTimeout(2000);
+    let overviewItems = await page.$$('[data-testid=overview] > ul > li');
+    let isDouble = false;
+    for (let i=0; i<overviewItems.length; i++) {
+        let overviewKey = await overviewItems[i].$$('span');
+        let overviewKeyText = await page.evaluate(el => el.textContent, overviewKey[0])
+        if (overviewKeyText.includes('Double Bedroom')) {
+            let overviewValue = await page.evaluate(el => el.textContent, overviewItems[0]);
+            if (overviewValue.includes('2')) {
+                isDouble = true;
+            }
+        }
+    }
+    page.close();
+    return isDouble;
 }
 
 function execute(config, callback) {
